56cd3e20d5253843bb3deb2bc7225f1dfcdf2ee7
vaishnavsm <vaishnavsm@gmail.com>
2018-01-27 10:44:54 +0530

    Add SQLAlchemy files and reqs

--- serv/microservices/geo/src/__init__.py
+++ serv/microservices/geo/src/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+import os
+
+DB_USERNAME = os.environ['POSTGRES_USERNAME']
+DB_PASSWORD = os.environ['POSTGRES_PASSWORD']
+DB_HOST = os.environ['POSTGRES_HOSTNAME']os.environ['POSTGRES_HOSTNAME']
+DB_PORT = os.environ['POSTGRES_PORT']
+DB_DB = "hasuradb"
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['DEVELOPMENT'] = True
+app.config['TESTING'] = True
+app.config['CSRF_ENABLED'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://%s:%s@%s:%s/%s"%(DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_DB)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # This line adds the hasura example routes form the hasura.py file.
 # Delete these two lines, and delete the file to remove them from your project
--- serv/microservices/geo/src/server.py
+++ serv/microservices/geo/src/server.py
@@ -1,10 +1,12 @@
 from src import app
+from flask.ext.sqlalchemy import SQLAlchemy
 # from flask import jsonify
 
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
-    return "Hasura Hello World"
+    return "Hasura Hello World<br>DB: %s"%db
 
 # Uncomment to add a new URL at /new
 
